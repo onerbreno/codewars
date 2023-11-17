5e3c59ce97e2e992ad4a235a3812fdfa9baf3d06
Breno <onerbreno@outlook.com>
2023-11-16 23:53:04 -0300

    Fundamentals - Add reverse words

---
+++ fundamentals/reverse_words/index.js
@@ -0,0 +1,17 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+function reverseWords(str) {
+  return str.split('').reverse().join('').split(' ').reverse().join(' ')
+}
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});
+
