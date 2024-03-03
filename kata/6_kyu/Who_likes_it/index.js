58b6acdb3530325830e7f250b6d698032c533132
Breno <onerbreno@outlook.com>
2024-03-02 21:58:00 -0300

    add katas

---
+++ kata/6_kyu/Who_likes_it/index.js
@@ -0,0 +1,32 @@
+
+
+function likes(names) {
+  const numLikes = names.length;
+  
+  switch (numLikes) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${numLikes - 2} others like this`;
+  }
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe('example tests', function() {
+  it('should return correct text', function() {
+    assert.strictEqual(likes([]), 'no one likes this');
+    assert.strictEqual(likes(['Peter']), 'Peter likes this');
+    assert.strictEqual(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+    assert.strictEqual(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+    assert.strictEqual(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+  });
+});
